858d8eae3acfc99d10f034f75f605a41873260c7
Josh Guo <joshua.guo@stonybrook.edu>
2019-06-24 12:45:21 -0400

    added scripts to support firestore

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyDU01SWkJGnAM1Hv27ekul9E0MagZT-sA0",
+	authDomain: "planit-041100.firebaseapp.com",
+	databaseURL: "https://planit-041100.firebaseio.com",
+	projectId: "planit-041100",
+	storageBucket: "",
+	messagingSenderId: "978009145818",
+	appId: "1:978009145818:web:8364d555100b53f9"
+	};
+		// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
